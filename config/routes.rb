d97025a2f13dde0e2ca2c7e87fb0efd22993a603
shibabu <shibabu.alfa@gmail.com>
2015-04-23 11:31:09 +0900

    scaffolded products and employees with references/joins to invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :purchases
+
+  resources :employees
+
   resources :companies do
     collection {
       post :import
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421171053) do
+ActiveRecord::Schema.define(version: 20150423022420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,34 @@ ActiveRecord::Schema.define(version: 20150421171053) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "job_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
     t.string   "company"
     t.decimal  "tax"
-    t.string   "salesperson"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status_type"
+    t.integer  "employee_id"
   end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.integer  "quantity"
+    t.integer  "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "purchases", ["invoice_id"], name: "index_purchases_on_invoice_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -57,4 +76,5 @@ ActiveRecord::Schema.define(version: 20150421171053) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "purchases", "invoices"
 end
